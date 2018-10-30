3a683f1f5cb1808b8e0b535faeea00fdccd34c9a
aditya <aditya.n.h02@gmail.com>
2018-10-30 20:35:55 +0700

    ubah

--- Show/index.php
+++ Show/index.php
@@ -15,6 +15,7 @@
   	<th>password</th>
   	<th>level</th>
   	<th>fullname</th>
+    <th>Action</th>
   </tr></div>
   <?php
   //  1. Lakukan include koneksi.php untuk membuat koneksi
@@ -24,16 +25,19 @@
   // 3. Jalankan perintah diatas dengan fungsi mysqli_query
   $peserta=mysqli_query($conn,$sql_tampil);
   // 4. Lakukan fetch dengan result type MYSQL_ASSOC
-  while($baris_data=mysqli_fetch_array($peserta,MYSQLI_ASSOC)){
-  	echo'
-  	</tr>
-  		<td>'.$baris_data['id'].'</td>
-  		<td>'.$baris_data['username'].'</td>
-  		<td>'.$baris_data['password'].'</td>
-  		<td>'.$baris_data['level'].'</td>
-  		<td>'.$baris_data['fullname'].'</td>
-  	</tr>';
-  }
+  while($baris_data=mysqli_fetch_array($peserta)){
+    ?>
+  	<tr>
+      <td align="center"><?php echo $baris_data['id']; ?></td>
+      <td><?php echo $baris_data['username']; ?></td>
+      <td><?php echo $baris_data['password']; ?></td>
+      <td><?php echo $baris_data['level']; ?></td>
+      <td><?php echo $baris_data['fullname']; ?></td>
+      <td align="center">
+        <a href="../ubah/ubah.php?id=<?php echo $baris_data['id']; ?>">Ubah</a>
+      </td>
+    </tr>
+  <?php }
   ?>
 </body>
 </html>
